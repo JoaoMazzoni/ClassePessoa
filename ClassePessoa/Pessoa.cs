f7bf21c5bbd84039e072678f6bc1b5830dafc777
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-30 11:18:00 -0300

    Add project files.

---
+++ ClassePessoa/Pessoa.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassePessoa
+{
+    internal class Pessoa //Classe do tipo internal
+    {
+        string Nome; //Variavel sem modificador, usa o padrão do C#, "internal"
+        int Idade;
+
+        public Pessoa() //Construtor
+        {
+
+        }
+
+        public Pessoa(string nome, int idade) //Construtor
+        {
+            this.Nome = nome; 
+            this.Idade = idade; 
+        }
+
+
+        public void setNome(string nome) //Método para receber um valor e atribuí-lo/inseri-lo no atributo "Nome"
+        {
+            this.Nome = nome;
+        }
+
+        public string getNome() //Método que acessa o atributo e pega o valor que está inserido lá dentro, podendo imprimi-lo ou processar uma ação.  
+        {
+            return this.Nome; //Retorna o valor que está dentro do atributo "Nome"
+        }
+
+        public void setIdade(int idade)
+        {
+            this.Idade = idade;
+        }
+
+        public void setIdade()
+        {
+            this.Idade = int.Parse(Console.ReadLine());
+        }
+
+        public int getIdade()
+        {
+            return this.Idade;
+        }
+
+    }
+}
---
+++ ClassePessoa/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassePessoa;
+
+Pessoa p1 = new Pessoa();
+
+Console.WriteLine();
+
+p1.setNome(Console.ReadLine()); //Console.ReadLine vai na referência, ou seja, está lendo a string digitada pelo usuário.
+Console.WriteLine(p1.getNome());
+
+p1.setIdade(int.Parse(Console.ReadLine())); //Método retornando o valor para o objeto 
+Console.WriteLine(p1.getIdade());
+
+Pessoa p2 = new Pessoa("Felipe", 39); //Inserindo valor sem o "set"
+Console.WriteLine(p2.getNome(), p2.getIdade()); //Acessando os atributos e pegando os valores que já foram inseridos neles.
